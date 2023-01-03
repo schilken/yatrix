ed477b0dceb0f33c05bf78a8e58e9fd50e1601a0
Alfred Schilken <alfred@schilken.de>
2023-01-03 16:27:09 +0100

    add flutter_riverpod and shared_preferences to prepare for preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tetris/tetris_game.dart';
 import 'pages/game_page.dart';
+import 'providers.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final sharedPreferences = await SharedPreferences.getInstance();
+
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-          child: Builder(builder: (context) {
-            return GamePage(
-                game: TetrisGame(
-            ),
-            );
-          },
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SafeArea(
+          child: Builder(
+            builder: (context) {
+              return GamePage(
+                game: TetrisGame(),
+              );
+            },
+          ),
         ),
         ),
-      ),
+    ),
   );
 }
