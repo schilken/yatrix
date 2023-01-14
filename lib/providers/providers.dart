4ede635978eb03e83618c163083a221b5b404544
Alfred Schilken <alfred@schilken.de>
2023-01-14 16:58:14 +0100

    rename PeerConnect* to PeerClient*

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -2,7 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 export 'high_score_notifier.dart';
-export 'peer_connect_notifier.dart';
+export 'peer_client_notifier.dart';
 export 'peer_server_notifier.dart';
 export 'preferences_repository.dart';
 export 'settings_notifier.dart';
