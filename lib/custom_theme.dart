9080299c736f33debc758322d81278e3a814820c
Alfred Schilken <alfred@schilken.de>
2023-01-18 13:22:15 +0100

    remove swatch and not needed colors

--- lib/custom_theme.dart
+++ lib/custom_theme.dart
@@ -2,36 +2,13 @@ import 'package:flutter/material.dart';
 
 class CustomPalette {
   static const Color almostBlack = Color(0xff040B13);
-  static const Color red100 = Color(0xfffce5e8);
-  static const Color red200 = Color(0xfff7b2ba);
-  static const Color red300 = Color(0xfff27f8d);
-  static const Color red400 = Color(0xffec4b5f);
-  static const Color red500 = Color(0xffE82139);
-  static const Color red600 = Color(0xffb41326);
-  static const Color red700 = Color(0xff800d1b);
-  static const Color red800 = Color(0xff4d0810);
-  static const Color red900 = Color(0xff1a0305);
 }
 
 class CustomTheme {
   static ThemeData darkTheme = ThemeData(
     colorScheme: ColorScheme.fromSwatch(
       brightness: Brightness.dark,
-      primarySwatch: MaterialColor(
-        CustomPalette.red500.value,
-        const {
-          100: CustomPalette.red100,
-          200: CustomPalette.red200,
-          300: CustomPalette.red300,
-          400: CustomPalette.red400,
-          500: CustomPalette.red500,
-          600: CustomPalette.red600,
-          700: CustomPalette.red700,
-          800: CustomPalette.red800,
-          900: CustomPalette.red900,
-        },
-      ),
-      accentColor: CustomPalette.red500,
+
     ),
     scaffoldBackgroundColor: CustomPalette.almostBlack,
     appBarTheme: const AppBarTheme(
@@ -71,7 +48,7 @@ class CustomTheme {
     ),
     textButtonTheme: TextButtonThemeData(
       style: TextButton.styleFrom(
-        backgroundColor: CustomPalette.red500,
+        backgroundColor: CustomPalette.almostBlack,
       ),
     ),
     // outlinedButtonTheme: OutlinedButtonThemeData(
